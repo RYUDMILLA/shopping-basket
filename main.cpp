ac1377b0c3e0b86162e6d116bc8f42d9199ea840
ryudmilla <dave.ryu218@gmail.com>
2022-04-08 15:19:12 +0900

    ESCAPE

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 typedef struct _Product{
     string category;
     string product_name;
-    int price;
+    int price{0};
 } Product;
 
 vector<Product> Products;
@@ -19,6 +19,19 @@ struct termios orig_raw;
 string filename;
 bool dirty{false};
 static string newfile_num{"1"};
+
+enum editorKey {
+    BACKSPACE = 127,
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP ,
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
+};
 
 // terminal
 
@@ -50,9 +63,50 @@ int readkey(void) {
     long nread;
     char c;
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-        if (nread == -1) exit(1);
+        if (nread == -1 && errno != EAGAIN) die("read");
     }
-    return c;
+
+    if (c == '\x1b'){   // escape
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+        if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                        case '1': return HOME_KEY;
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
+                        case '3': return DEL_KEY;
+                    }
+                  }
+                } else {
+                    switch (seq[1]) {
+                        case 'A': return ARROW_UP;
+                        case 'B': return ARROW_DOWN;
+                        case 'C': return ARROW_RIGHT;
+                        case 'D': return ARROW_LEFT;
+                        case 'H': return HOME_KEY;
+                        case 'F': return END_KEY;
+                    }
+                }
+            } else if (seq[0] == '0') {
+                switch (seq[1]) {
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
+                }
+            }
+
+        return '\x1b';
+    } else {
+        return c;
+    }
 }
 
 // Functions
@@ -64,10 +118,33 @@ long search_name(string name) {
     return -1;
 }
 
+bool is_number(const string& s)
+{
+    return !s.empty() && find_if(s.begin(), s.end(), [](unsigned char c) { return !isdigit(c); }) == s.end();
+}
+
 void add_product(void) {
     Product new_product;
-    cout << "Enter product: ";
+    write(STDOUT_FILENO,"Enter Product: ",15);
+    long nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    if (c == 27) {
+        disableRawmode();
+        return;
+    }
+    write(STDOUT_FILENO, &c, 1);
+    disableRawmode();
     getline(cin, new_product.product_name);
+    new_product.product_name = c + new_product.product_name;
+    long idx = search_name(new_product.product_name);
+    if (idx != -1) {
+        cout << "Already exists. (num: " << idx << ")" << endl;
+        return;
+    }
+    
     cout << "Enter price: ";
     scanf("%d",&new_product.price);
     cout << "Enter category: ";
@@ -79,16 +156,22 @@ void add_product(void) {
 }
 
 void delete_product(void) {
-    string name;
+    string s;
     cout << "Which one to delete? ";
-    getline(cin, name);
-    long idx {search_name(name)};
-    if (idx != -1) {
+    getline(cin, s);
+    long idx;
+    
+    if (is_number(s))
+        idx = stoi(s) - 1;
+    else
+        idx = search_name(s);
+    
+    if (idx >= 0 && idx < Products.end() - Products.begin()) {
         Products.erase(Products.begin() + idx);
         dirty = true;
-        cout << name << " Deleted." << endl;
+        cout << Products[idx].product_name << " Deleted." << endl;
     } else {
-        cout << name << " Not found." << endl;
+        cout << Products[idx].product_name << " Not found." << endl;
     }
 }
 
@@ -96,23 +179,37 @@ void find_product(void) {
     string name;
     cout << "Enter the product name: ";
     getline(cin, name);
-    long idx {search_name(name)};
-    if (idx != -1) {
-        cout << idx+1 << ". " << name << " / Price: " << put_money(Products[idx].price) << " / Category: " << Products[idx].category << endl;
-    } else {
+    vector<size_t> idx;
+    
+    for (vector<Product>::iterator itr = Products.begin(); itr != Products.end(); ++itr)
+        if (((*itr).product_name).find(name) != string::npos)
+            idx.push_back(itr-Products.begin());
+    
+    if (idx.empty())
         cout << name << " Not found." << endl;
+    else {
+        cout << '\n';
+        for (size_t i : idx) {
+            cout << i+1 << ". " << Products[i].product_name << " / Price: " << put_money(Products[i].price) << " / Category: " << Products[i].category << endl;
+        }
     }
 }
 
 void modify_product(void) {
-    string name;
-    cout << "Enter the product to modify: ";
-    getline(cin, name);
-    long idx {search_name(name)};
-    if (idx != -1) {
+    string s;
+    cout << "Enter the product name or number to modify: ";
+    getline(cin, s);
+    long idx;
+    
+    if (is_number(s))
+        idx = stoi(s) - 1;
+    else
+        idx = search_name(s);
+    
+    if (idx >= 0 && idx < Products.end() - Products.begin()) {
         string m;
         cout << "\n### Press Enter not to modify ###\n" << endl;
-        cout << "(Existing: " << name << ")" << endl;
+        cout << "(Existing: " << Products[idx].product_name << ")" << endl;
         cout << "Enter name: ";
         getline(cin, m);
         if (!empty(m))
@@ -132,18 +229,16 @@ void modify_product(void) {
         dirty = true;
         cout << '\n' << idx+1 << ". " << Products[idx].product_name << " / Price: " << put_money(Products[idx].price) << " / Category: " << Products[idx].category << "  Modified." << endl;
     } else {
-        cout << name << " Not found." << endl;
+        cout << s << " Not found." << endl;
     }
 }
 
 void list_product(void) {
-    cout << right << setw(11) << "Product" << setw(35) << "Price" << setw(13) << "Category" << endl;
+    cout << "\n" << right << setw(11) << "Product" << setw(35) << "Price" << setw(13) << "Category" << endl;
     for (vector<Product>::iterator itr = Products.begin(); itr != Products.end(); ++itr) {
         cout << left << setw(4) << (itr-Products.begin()+1) << setw(37) << (*itr).product_name << setw(10)<< put_money((*itr).price) << (*itr).category << endl;
     }
 }
-
-
 
 // File I/O
 
@@ -203,15 +298,15 @@ void save_file(void) {
 
 void keyprocess(void) {
     static int quit_times {QUIT_TIMES};
-    
+    write(STDOUT_FILENO, "\x1b[1E", 4);
     cout << "\nCtrl + A : Add / D : Delete / M : Modify / F : Find / L : List / O : Open / S : Save / Q : Quit" << endl;
-//    cout << "Enter the key: ";
+    write(STDOUT_FILENO, "\x1b[1E", 4);
     
     long c = readkey();
     
     switch (c) {
         case CTRL_KEY('a'):
-            disableRawmode();
+//            disableRawmode();
             add_product();
             enableRawmode();
             break;
@@ -231,7 +326,9 @@ void keyprocess(void) {
             enableRawmode();
             break;
         case CTRL_KEY('l'):
+            disableRawmode();
             list_product();
+            enableRawmode();
             break;
         case CTRL_KEY('o'):
             disableRawmode();
@@ -247,13 +344,20 @@ void keyprocess(void) {
             if (dirty == true) {
                 cout << "File has unsaved changes." << endl;
                 while (quit_times > 0) {
-                    cout << "Enter Q " << quit_times << " more time to quit.";
+                    write(STDOUT_FILENO, "\x1b[1E", 4);
+                    cout << "Press Ctrl+Q " << quit_times << " more time to quit. Or Ctrl+S to save." << endl;
+                    write(STDOUT_FILENO, "\x1b[1E", 4);
                     c = readkey();
-                    if (c != CTRL_KEY('q'))
+                    if (c == CTRL_KEY('s')) {
+                        save_file();
+                        return;
+                    }
+                    else if (c != CTRL_KEY('q'))
                         return;
                     quit_times--;
                 }
             }
+            write(STDOUT_FILENO, "\x1b[1E", 4);
             cout << "Program terminated." << endl;
             exit(1);
         default:
